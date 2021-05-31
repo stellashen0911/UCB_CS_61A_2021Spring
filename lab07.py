f0ab20c46fafef81e1946c69b9a0d2c16e6232de
stellashen0911 <60760608+stellashen0911@users.noreply.github.com>
2021-05-31 17:43:10 +0800

    Add files via upload

---
+++ lab07.py
@@ -0,0 +1,213 @@
+def convert_link(link):
+    """Takes a linked list and returns a Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> convert_link(link)
+    [1, 2, 3, 4]
+    >>> convert_link(Link.empty)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    'base case'
+    if link is link.empty:
+        return []
+    return [link.first] + convert_link(link.rest)
+
+
+def cumulative_mul(t):
+    """Mutates t so that each node's label becomes the product of all labels in
+    the corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_mul(t)
+    >>> t
+    Tree(105, [Tree(15, [Tree(5)]), Tree(7)])
+    """
+    "*** YOUR CODE HERE ***"
+    if t.is_leaf:
+        return t
+    for b in t.branches:
+        cumulative_mul(b)
+    total = t.label
+    for b in t.branches:
+        total *= b.label
+    t.lable = total
+
+def has_cycle(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle(t)
+    False
+    >>> u = Link(2, Link(2, Link(2)))
+    >>> has_cycle(u)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    'the insight here is to create a new list, and compare the original link list with the new list'
+    new_link = []
+    while link is not Link.empty:
+        if link in new_link:
+            return True
+        new_link.append(link.first)
+        link = link.rest
+    return False
+
+
+def has_cycle_constant(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle_constant(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle_constant(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if link is Link.empty:
+        return False
+    short, long = link, link.rest
+    while long is not Link.empty:
+        if long.rest == Link.empty:
+            return False
+        elif long.rest is short or long is short:
+            return True
+        else:
+            short , long = short.rest, long.rest.rest
+    return False
+
+def every_other(s):
+    """Mutates a linked list so that all the odd-indiced elements are removed
+    (using 0-based indexing).
+
+    >>> s = Link(1, Link(2, Link(3, Link(4))))
+    >>> every_other(s)
+    >>> s
+    Link(1, Link(3))
+    >>> odd_length = Link(5, Link(3, Link(1)))
+    >>> every_other(odd_length)
+    >>> odd_length
+    Link(5, Link(1))
+    >>> singleton = Link(4)
+    >>> every_other(singleton)
+    >>> singleton
+    Link(4)
+    """
+    "*** YOUR CODE HERE ***"
+    #if the length of the linked list is two or less, return the same linked list result
+    if s is Link.empty or s.rest is Link.empty:
+        return 
+    #the rest case is when length of linked list > 2
+    #the insight here is "odd-indice" and delete the "link.first"
+    else:
+        s.rest = s.rest.rest
+        every_other(s.rest)
+
+def reverse_other(t):
+    """Mutates the tree such that nodes on every other (odd-depth) level
+    have the labels of their branches all reversed.
+
+    >>> t = Tree(1, [Tree(2), Tree(3), Tree(4)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(4), Tree(3), Tree(2)])
+    >>> t = Tree(1, [Tree(2, [Tree(3, [Tree(4), Tree(5)]), Tree(6, [Tree(7)])]), Tree(8)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(8, [Tree(3, [Tree(5), Tree(4)]), Tree(6, [Tree(7)])]), Tree(2)])
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(t, need_reverse):
+        if t.is_leaf:
+            return
+    #reverse the branch list
+        new_list = [b.label for b in t.branches][::-1]
+        for i in range(len(t.branches)):
+            b = t.branches[i]
+            helper(b, not need_reverse)
+            if need_reverse:
+                b.label = new_list[i]
+    return helper(t, True)
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
